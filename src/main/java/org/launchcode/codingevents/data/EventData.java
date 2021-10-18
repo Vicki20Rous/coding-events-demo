5404cc092c16e400431b2b08f522ae61bbe213ea
Vicki20rous <vic86rous@gmail.com>
2021-10-18 07:39:58 -0500

    added validation

--- src/main/java/org/launchcode/codingevents/data/EventData.java
+++ src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -11,21 +11,25 @@ public class EventData {
 
     // get all events
     public static Collection<Event> getAll() {
+
         return events.values();
     }
 
     // get a single event
     public static Event getById(int id) {
+
         return events.get(id);
     }
 
     // add an event
     public static void add(Event event) {
+
         events.put(event.getId(), event);
     }
 
     // remove an event
     public static void remove(int id) {
+
         events.remove(id);
     }
 }
